241e0130ac9236ab1b87db7bf6c36ffaa11bf07f
figandgrapeio <maxzanuttini@figandgrape.io>
2022-04-21 01:04:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './components/images/logo-cmsec.png';
+import underconstructionimage from './components/images/NicePng_under-construction-png_1526045.png';
+import {
+  VStack,
+  Link,
+  Image,
+  Text,
+  Center,
+  useColorModeValue,
+  Button,
+} from '@chakra-ui/react';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const Render = () => {
 }
 
-export default App;
+const IndexPage = () => {
+  return (<>
+    <Center bgGradient="linear(to-r, blackAlpha.500, gray.500, whiteAlpha.700, gray.500, blackAlpha.500)" m='4'>
+      <VStack w='500px' h='500px' justify={'center'}><Image src={logo} /></VStack>
+    </Center>
+    <VStack spacing='4' py={1} bgGradient="linear(to-r, orange.500, orange.700)" m='4' w={'auto'}>
+      <Text fontSize={'3xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}
+      >Puede contactarnos <Button variant='link' color={useColorModeValue('orange.500', 'orange.500')} fontSize={'3xl'} as='a' href='mailto:contacto@conmatsecurity.net'>aquí</Button></Text>
+      <Text fontSize={'3xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}
+      >To contact us, you can do it <Button variant='link' color={useColorModeValue('orange.500', 'orange.500')} fontSize={'3xl'} as='a' href='mailto:contacto@conmatsecurity.net'>here</Button></Text>
+    </VStack>
+    <VStack mt={24}>
+    <Center bgGradient="linear(to-r, blackAlpha.500, gray.500, whiteAlpha.700, gray.500, blackAlpha.500)" m='4'>
+      <VStack w='auto' h='auto' justify={'center'}><Image src={underconstructionimage} /></VStack>
+    </Center>
+      <Text fontSize={'xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}>Sitio en construcción</Text>
+      {/* <Text fontSize={'xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}>Site is under construction</Text> */}
+    </VStack>
+    <VStack mt={12}>
+    <Text fontSize={'sm'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')} >
+          <Link href="https://figandgrape.io" textAlign={"center"}>
+          Site powered By Fig &amp; Grape<br />
+          </Link>
+          Copyright &copy; {new Date().getUTCFullYear()} 
+        </Text>
+    </VStack>
+  </>
+  );
+}
+export default IndexPage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import IndexPage from './App';
+import { ChakraProvider, extendTheme, theme as base } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: '#f5fee5',
+      100: '#e1fbb2',
+      200: '#cdf781',
+      300: '#b8ee56',
+      400: '#a2e032',
+      500: '#8ac919',
+      600: '#71ab09',
+      700: '#578602',
+      800: '#3c5e00',
+      900: '#203300',
+    },
+  },
+  fonts: {
+    heading: `Montserrat, ${base.fonts?.heading}`,
+    body: `Inter, ${base.fonts?.heading}`,
+    footer: `Montserrat, ${base.fonts?.footer}`
+  },
+  config: {
+    initialColorMode: 'dark'
+  },
+
+});
+export default theme;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <IndexPage />
+    </ChakraProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
