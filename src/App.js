a7ea736ac90e6a711dc1a7a05c2116b3c4b519ef
figandgrape <maxzanuttini@figandgrape.io>
2022-04-22 12:49:19 -0300

    .

--- src/App.js
+++ src/App.js
@@ -21,15 +21,15 @@ const IndexPage = () => {
     </Center>
     <VStack spacing='4' py={1} bgGradient="linear(to-r, blackAlpha.500, gray.500, whiteAlpha.700, gray.500, blackAlpha.500)" m='4'>
       <Text fontSize={'3xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}
-      >Su seguridad es nuestra mayor preocupación</Text>
-      <Text fontSize={'3xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}
-      >Your safety is our most important concern </Text>
+      >Su tranquilidad es nuestra mayor preocupación</Text>
+      {/* <Text fontSize={'3xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}
+      >Your safety is our most important concern </Text> */}
     </VStack>
     <VStack spacing='4' py={1} bgGradient="linear(to-r, orange.500, orange.700)" m='4' w={'auto'}>
       <Text fontSize={'3xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}
       >Puede contactarnos <Button variant='link' color={useColorModeValue('orange.500', 'orange.500')} fontSize={'3xl'} as='a' href='mailto:contacto@conmatsecurity.net'>aquí</Button></Text>
-      <Text fontSize={'3xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}
-      >To contact us, you can do it <Button variant='link' color={useColorModeValue('orange.500', 'orange.500')} fontSize={'3xl'} as='a' href='mailto:contacto@conmatsecurity.net'>here</Button></Text>
+      {/* <Text fontSize={'3xl'} fontStyle='oblique' textAlign={'center'} textTransform='uppercase' color={useColorModeValue('black', 'white')}
+      >To contact us, you can do it <Button variant='link' color={useColorModeValue('orange.500', 'orange.500')} fontSize={'3xl'} as='a' href='mailto:contacto@conmatsecurity.net'>here</Button></Text> */}
     </VStack>
     <VStack mt={24}>
     <Center bgGradient="linear(to-r, blackAlpha.500, gray.500, whiteAlpha.700, gray.500, blackAlpha.500)" m='4'>
